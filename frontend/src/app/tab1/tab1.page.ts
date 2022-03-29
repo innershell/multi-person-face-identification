8e41c9a2e6cc046db4cf13e2aacb4ae72a4e319f
Melvin Tan <innershell@gmail.com>
2022-03-28 23:33:30 -0600

    Photo upload working.

--- frontend/src/app/tab1/tab1.page.ts
+++ frontend/src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor(public photoService: PhotoService) {}
+
+  ngOnInit() {
+    this.photoService.capturePhoto();
+  }
+
+  photoButtonClick() {
+    this.photoService.capturePhoto();
+  }
 
 }
